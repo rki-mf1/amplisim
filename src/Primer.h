5772d358ff4cd777ca21e7136cb9e25e7302490c
Thomas Krannich <thomas@Thomass-MBP-2.local>
2023-08-02 16:45:13 +0200

    restructure the Primer class

--- src/Primer.h
+++ src/Primer.h
@@ -19,22 +19,6 @@ class Primer{
 };
 
 
-/**
- * @brief Construct a new Primer object
- * 
- * @param chr The chromosome of the primer.
- * @param start_left The start position of the left primer.
- * @param end_left The end position of the left primer.
- * @param start_right The start position of the right primer.
- * @param end_right The end position of the right primer.
- */
-Primer::Primer(std::string chr, int start_left, int end_left, int start_right, int end_right){
-    this->chr = chr;
-    this->start_left = start_left;
-    this->end_left = end_left;
-    this->start_right = start_right;
-    this->end_right = end_right;
-}
 
 
 #endif // PRIMER_H
