74de9f3d7c15c89cef3f4734cd584a25426b1938
Thomas Krannich <thomas@Thomass-MacBook-Pro-2.local>
2023-08-03 17:04:19 +0200

    code refactorization; FASTA parser for the amplicons

--- src/amplisim.cpp
+++ src/amplisim.cpp
@@ -1,150 +1,13 @@
 // Purpose: Main file for the amplisim program.
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <unordered_map>
-#include <string>
-#include <sstream>
-#include <cassert>
-#include <htslib/faidx.h>
 
 #include "Primer.h"
 #include "PrimerIndex.h"
 #include "AmpliconGenerator.h"
+#include "util.h"
 
 
 
-/**
- * @brief Read a FASTA file and store the names and sequences in vectors.
- * 
- * @param fasta_file The name of the FASTA file.
- * @param chromosomes An unordered map to store the names (key) and sequences (value) of the chromosomes.
- * @return int 0 if the function was executed correctly, 1 otherwise.
- */
-int read_fasta(std::string fasta_ref_gemome, std::unordered_map<std::string, std::string> &chromosomes){
 
-    // open the fasta file
-    faidx_t *fai = fai_load(fasta_ref_gemome.c_str());
-
-    // check if the file was opened correctly
-    if (fai == NULL){
-        std::cerr << "Error opening the FASTA/FAI file." << std::endl;
-        return 1;
-    }
-
-    // print a message to the user
-    std::cout << "Reading the referece FASTA file..." << std::endl;
-
-    // get the number of sequences in the fasta file
-    int n_seqs = faidx_nseq(fai);
-
-    // from the multi FASTA file get all the chromosome names and sequences and store them in vectors
-    for (int i = 0; i < n_seqs; i++){
-
-        // get the chromosome name
-        const char *name = faidx_iseq(fai, i);
-
-        // get chromosome end position
-        int end = faidx_seq_len(fai, name);
-
-        // get the chromosome sequence
-        int len;
-        char *seq = faidx_fetch_seq(fai, name, 0, end, &len);
-
-        // store the chromosome name and sequence in the unordered map
-        chromosomes[name] = seq;
-
-        // free the memory
-        free(seq);
-    }
-
-    // close the fasta file
-    fai_destroy(fai);
-
-    // assert that the chromosomes map is not empty
-    assert(!chromosomes.empty());
-
-    return 0;
-
-}
-
-
-/**
- * @brief Read a BED file and store the information in a vector of Primer objects.
- * 
- * @param bed_file The name of the BED file.
- * @param primers A vector of Primer objects to store the information of the primers.
- * @return int 0 if the function was executed correctly, 1 otherwise.
- */
-int read_bed(std::string bed_file, std::vector<Primer> &primers){
-
-    // open the BED file
-    std::ifstream bed(bed_file);
-
-    // check if the file was opened correctly
-    if (!bed.is_open()){
-        std::cerr << "Error opening the BED file." << std::endl;
-        return 1;
-    }
-
-    // print a message to the user
-    std::cout << "Reading the primer BED file..." << std::endl;
-
-    // define a string to store the current line
-    std::string line;
-
-    /* Loop over the lines of the BED file (amplicaon primers).
-       Every odd line            : information of the left primer
-       Every even line           : information of the right primer
-       Every two lines (odd line): create a Primer object and store it in the vector
-    */
-    int i = 0;
-    std::string chrom;
-    int left_start, left_end, right_start, right_end;
-
-    while (std::getline(bed, line) && !line.empty()){       // avoid getting trapped in eof artefacts (empty lines)
-
-        // split the line by tab
-        std::vector<std::string> fields;
-        std::stringstream ss(line);
-        std::string field;
-        while (std::getline(ss, field, '\t')){
-            fields.push_back(field);
-        }
-
-        // check if the line number is odd or even
-        if (i & 1){
-            // if the last bit of i is set, we are reading the second line of the primer
-            // check if the chromosome is the same as the previous line
-            if (fields[0] != chrom){
-                std::cerr << "Error: the chromosome of the left and right primer are not the same." << std::endl;
-                return 1;
-            }            
-            right_start = std::stoi(fields[1]);
-            right_end = std::stoi(fields[2]);
-
-            // create a Primer object and store it in the vector
-            Primer p(chrom, left_start, left_end, right_start, right_end);
-            primers.push_back(p);
-
-        } else {
-            // if the last bit of i is not set, we are reading the first line of the primer
-            // store the information of the left primer
-            chrom = fields[0];
-            left_start = std::stoi(fields[1]);
-            left_end = std::stoi(fields[2]);
-        }
-
-        // increase the line counter
-        i++;
-    }
-
-    // close the BED file
-    bed.close();
-
-    return 0;
-
-}
 
 
 int main(int argc, char const *argv[]){
@@ -165,8 +28,8 @@ int main(int argc, char const *argv[]){
     // create an unordered map to store the names (key) and sequences (value) of the chromosomes
     std::unordered_map<std::string, std::string> chromosomes;
 
-    // call the read_fasta function and catch the return value
-    int ret = read_fasta(ref_genome, chromosomes);
+    // call the read_reference function and catch the return value
+    int ret = read_reference(ref_genome, chromosomes);
     // check if the function was executed correctly
     if (ret != 0){
         std::cerr << "Error reading the reference FASTA file." << std::endl;
@@ -179,8 +42,8 @@ int main(int argc, char const *argv[]){
     // define a vector of Primer objects
     std::vector<Primer> primers;
     
-    // call the read_bed function and catch the return value
-    ret = read_bed(bed_file, primers);
+    // call the read_primer function and catch the return value
+    ret = read_primer(bed_file, primers);
     // check if the function was executed correctly
     if (ret != 0){
         std::cerr << "Error reading the primer BED file." << std::endl;
@@ -210,6 +73,8 @@ int main(int argc, char const *argv[]){
         return 1;
     }
 
+    // use the write_amplicons function to write the amplicons to a file
+    ret = write_amplicons("amplicons.fa", amplicons);
     
     return 0;
 }
--- src/util.h
+++ src/util.h
@@ -2,7 +2,15 @@
 #define UTIL_H
 
 #include <string>
+#include <vector>
+#include <unordered_map>
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <htslib/faidx.h>
+
+#include "Primer.h"
 
 
 /**
@@ -38,6 +46,179 @@ static std::string reverse_complement(std::string seq){
 
     return rev_comp;
 }
+
+
+/**
+ * @brief Read a reference FASTA file and store the names and sequences in vectors.
+ * 
+ * @param fasta_file The name of the FASTA file.
+ * @param chromosomes An unordered map to store the names (key) and sequences (value) of the chromosomes.
+ * @return int 0 if the function was executed correctly, 1 otherwise.
+ */
+static int read_reference(std::string fasta_ref_gemome, std::unordered_map<std::string, std::string> &chromosomes){
+
+    // open the fasta file
+    faidx_t *fai = fai_load(fasta_ref_gemome.c_str());
+
+    // check if the file was opened correctly
+    if (fai == NULL){
+        std::cerr << "Error opening the FASTA/FAI file." << std::endl;
+        return 1;
+    }
+
+    // print a message to the user
+    std::cout << "Reading the referece FASTA file..." << std::endl;
+
+    // get the number of sequences in the fasta file
+    int n_seqs = faidx_nseq(fai);
+
+    // from the multi FASTA file get all the chromosome names and sequences and store them in vectors
+    for (int i = 0; i < n_seqs; i++){
+
+        // get the chromosome name
+        const char *name = faidx_iseq(fai, i);
+
+        // get chromosome end position
+        int end = faidx_seq_len(fai, name);
+
+        // get the chromosome sequence
+        int len;
+        char *seq = faidx_fetch_seq(fai, name, 0, end, &len);
+
+        // store the chromosome name and sequence in the unordered map
+        chromosomes[name] = seq;
+
+        // free the memory
+        free(seq);
+    }
+
+    // close the fasta file
+    fai_destroy(fai);
+
+    // assert that the chromosomes map is not empty
+    assert(!chromosomes.empty());
+
+    return 0;
+
+}
+
+
+/**
+ * @brief Read a primer BED file and store the information in a vector of Primer objects.
+ * 
+ * @param bed_file The name of the BED file.
+ * @param primers A vector of Primer objects to store the information of the primers.
+ * @return int 0 if the function was executed correctly, 1 otherwise.
+ */
+static int read_primer(std::string bed_file, std::vector<Primer> &primers){
+
+    // open the BED file
+    std::ifstream bed(bed_file);
+
+    // check if the file was opened correctly
+    if (!bed.is_open()){
+        std::cerr << "Error opening the BED file." << std::endl;
+        return 1;
+    }
+
+    // print a message to the user
+    std::cout << "Reading the primer BED file..." << std::endl;
+
+    // define a string to store the current line
+    std::string line;
+
+    /* Loop over the lines of the BED file (amplicaon primers).
+       Every odd line            : information of the left primer
+       Every even line           : information of the right primer
+       Every two lines (odd line): create a Primer object and store it in the vector
+    */
+    int i = 0;
+    std::string chrom;
+    int left_start, left_end, right_start, right_end;
+
+    while (std::getline(bed, line) && !line.empty()){       // avoid getting trapped in eof artefacts (empty lines)
+
+        // split the line by tab
+        std::vector<std::string> fields;
+        std::stringstream ss(line);
+        std::string field;
+        while (std::getline(ss, field, '\t')){
+            fields.push_back(field);
+        }
+
+        // check if the line number is odd or even
+        if (i & 1){
+            // if the last bit of i is set, we are reading the second line of the primer
+            // check if the chromosome is the same as the previous line
+            if (fields[0] != chrom){
+                std::cerr << "Error: the chromosome of the left and right primer are not the same." << std::endl;
+                return 1;
+            }            
+            right_start = std::stoi(fields[1]);
+            right_end = std::stoi(fields[2]);
+
+            // create a Primer object and store it in the vector
+            Primer p(chrom, left_start, left_end, right_start, right_end);
+            primers.push_back(p);
+
+        } else {
+            // if the last bit of i is not set, we are reading the first line of the primer
+            // store the information of the left primer
+            chrom = fields[0];
+            left_start = std::stoi(fields[1]);
+            left_end = std::stoi(fields[2]);
+        }
+
+        // increase the line counter
+        i++;
+    }
+
+    // close the BED file
+    bed.close();
+
+    return 0;
+
+}
+
+
+/**
+ * @brief Write the amplicons to a FASTA file.
+ * 
+ * @param fasta_file The name of the FASTA file.
+ * @param amplicons A vector of strings containing the amplicons.
+ * @return int 0 if the function was executed correctly, 1 otherwise.
+ */
+static int write_amplicons(std::string fasta_file, std::vector<std::string> &amplicons){
+
+    // open the FASTA file
+    std::ofstream fasta(fasta_file);
+
+    // check if the file was opened correctly
+    if (!fasta.is_open()){
+        std::cerr << "Error opening the FASTA file." << std::endl;
+        return 1;
+    }
+
+    // print a message to the user
+    std::cout << "Writing the amplicons to a FASTA file..." << std::endl;
+
+    // cast the size of the amplicons vector an int
+    int n_amplicons = amplicons.size();
+
+    // loop over the amplicons and write them to the FASTA file
+    for (int i = 0; i < n_amplicons; i++){
+        fasta << ">amplicon_" << i << std::endl;
+        fasta << amplicons[i] << std::endl;
+    }
+
+    // close the FASTA file
+    fasta.close();
+
+    return 0;
+
+}
+
+
 
 
 
