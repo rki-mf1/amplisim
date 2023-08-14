80ab69b01ac4c2dc9d5c60a67d00b1fb07084b65
Thomas Krannich <thomas@Thomass-MacBook-Pro-2.local>
2023-08-14 15:48:11 +0200

    output only in verbose mode

--- src/amplisim.cpp
+++ src/amplisim.cpp
@@ -26,23 +26,20 @@ int main(int argc, char *argv[]){
     std::string bed_file   = arguments.args[1];
 
     // print a message to the user
-    std::cout << "Running amplisim..." << std::endl;
-    std::cout << "=== Arguments =====" << std::endl;
-    std::cout << "Reference genome: " << ref_genome << std::endl;
-    std::cout << "Primer BED file : " << bed_file   << std::endl;
-    std::cout << "Seed            : " << arguments.seed << std::endl;
-    if (arguments.output_file == NULL){
-        std::cout << "Output file     : amplicons.fa" << std::endl;
-    }
-    else{
+    if (arguments.verbose){
+        std::cout << "=== Arguments =====" << std::endl;
+        std::cout << "Reference genome: " << ref_genome << std::endl;
+        std::cout << "Primer BED file : " << bed_file   << std::endl;
+        std::cout << "Seed            : " << arguments.seed << std::endl;
         std::cout << "Output file     : " << arguments.output_file << std::endl;
+        std::cout << "===================" << std::endl << std::endl;
+        std::cout << "\033[32;40mStarting\033[0m amplisim..." << std::endl;
     }
-    std::cout << "===================" << std::endl;
 
     // create an unordered map to store the names (key) and sequences (value) of the chromosomes
     std::unordered_map<std::string, std::string> chromosomes;
 
-    int ret = read_reference(ref_genome, chromosomes);
+    int ret = read_reference(ref_genome, chromosomes, arguments.verbose);
     if (ret != 0){
         std::cerr << "Error reading the reference FASTA file." << std::endl;
         return 1;
@@ -51,26 +48,21 @@ int main(int argc, char *argv[]){
     // define a vector of Primer objects
     std::vector<Primer> primers;
 
-    ret = read_primer(bed_file, primers);
+    ret = read_primer(bed_file, primers, arguments.verbose);
     if (ret != 0){
         std::cerr << "Error reading the primer BED file." << std::endl;
         return 1;
     }
 
     // create a PrimerIndex object
-    PrimerIndex primer_index(primers);
-
-    // [COUT] for all chromosomes, print get_index and get_runlength from the PrimerIndex object (tab separated)
-    for (auto const &chr : chromosomes){
-        std::cout << chr.first << "\t" << primer_index.get_index(chr.first) << "\t" << primer_index.get_runlength(chr.first) << std::endl;
-    }
+    PrimerIndex primer_index(primers, arguments.verbose);
 
     // create an empty vector of strings to store the amplicons
     std::vector<std::string> amplicons;
 
     // create an amplicons
     AmpliconGenerator amplicon_generator(primers, chromosomes, primer_index);
-    ret = amplicon_generator.generate_amplicons(amplicons);
+    ret = amplicon_generator.generate_amplicons(amplicons, arguments.verbose);
     if (ret != 0){
         std::cerr << "Error generating the amplicons." << std::endl;
         return 1;
@@ -78,12 +70,20 @@ int main(int argc, char *argv[]){
 
     // vec_reps is a vector of ints that stores the number of amplifications for each amplicon template sequence
     std::vector<int> vec_reps = amplicon_generator.get_vec_reps();
+
     assert(vec_reps.size() > 0);
     assert(std::accumulate(vec_reps.begin(), vec_reps.end(), 0) == amplicons.size());
 
     // use the write_amplicons function to write the amplicons to a file
-    ret = write_amplicons("amplicons.fa", amplicons, vec_reps);
+    ret = (arguments.output_file == NULL ) ? write_amplicons(NULL, amplicons, vec_reps) : write_amplicons(arguments.output_file, amplicons, vec_reps, false);
+    if (ret != 0){
+        std::cerr << "Error writing the amplicons to a file." << std::endl;
+        return 1;
+    }
     
-
+    // print a message to the user
+    if (arguments.verbose){
+        std::cout << "\033[32;40mFinished\033[0m with exit status 0." << std::endl;
+    }
     return 0;
 }
